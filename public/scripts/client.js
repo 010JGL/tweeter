5ce80f83a1ca084d655322cafedd3260c5d8dba1
Jeremy <010JGL@GMAIL.COM>
2023-03-30 13:28:55 -0400

    Add a feature to render tweets in the right order with reverse(). Add a function to make the input from user safe from modifying our scripts.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -47,6 +47,15 @@ const data = [
 $(document).ready(function () {
 
   const createTweetElement = function (tweetObj) {
+    // function to make the text safe so a user cannot affect my script
+    const escape = function (str) {
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML
+    };
+    // save the value in a const, to use it in our HTML
+    const safeHTML = `<p>${escape(tweetObj.content.text)}</p>`;
+    
     // copy the whole article so we have the same structure for every tweet
     // line 53 imports timeago in script. for line 63
     const $tweet = $(`
@@ -57,7 +66,7 @@ $(document).ready(function () {
         <h2>${tweetObj.user.handle}
         </h2>
       </header>
-      <p>${tweetObj.content.text}</p>
+      <p>${safeHTML}</p>
       <footer class="bot-container">
         <p class="bottom-left">${timeago.format(tweetObj.created_at)}</span>
         </p>
@@ -73,6 +82,9 @@ $(document).ready(function () {
   }
 
   const renderTweets = function (tweets) {
+    // everytime we render, its gonna empty the child of the parent class .tweet-container
+    // this is so we dont load every tweet everytime we post a new one
+    $('.tweet-container').empty();
     // loops through tweets
     for (let tweet of tweets) {
       // calls createTweetElement for each tweet
@@ -81,13 +93,12 @@ $(document).ready(function () {
   }
   //renderTweets(data);
 
-//linking the form, when we click submit, to an event
+  //linking the form, when we click submit, to an event
   $("form").on("submit", function (event) {    
     event.preventDefault();                    //preventing to go to page /tweets
     const message = $(this).serialize()         // turn form data into a query string
-    //console.log('message.length', message.length)       
+       
     if (message.length > 5 && message.length <= 145) {    // not optimal, need to find improvement
-      
       $.ajax('/tweets', {
         type: "POST",
         data: message,
@@ -97,19 +108,21 @@ $(document).ready(function () {
         loadtweets();
       })
     } else {
-    alert("No tweet input or too many characters")
+      alert("No tweet input or too many characters")
     }
     
   });
-
-
+  
   const loadtweets = function() {
     $.ajax('/tweets', { method: 'GET'})
     .then(function (tweet) {
+      // takes the tweet and reverse it so the last tweet is on top
+      tweet.reverse()
       renderTweets(tweet)
       
     })
   }
   loadtweets();
+  
 });
 
